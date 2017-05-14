c95d250a16881a61acba16f0793426fb4d4900ca
hkociemba <kociemba@t-online.de>
2017-05-14 16:27:02 +0200

    Add files via upload

--- example.py
+++ example.py
@@ -1,13 +1,19 @@
 # ############################ Examples how to use the cube solver #####################################################
 
-cubestring = 'FFBLBRDLDUBRRFDDLRLUUUFB'  # cube definition string of cube we want to solve
+cubestring = 'DUUBULDBFRBFRRULLLBRDFFFBLURDBFDFDRFRULBLUFDURRBLBDUDL'  # cube definition string of cube we want to solve
 # See module enums.py for the format of the cube definition string
 
 # ######################### Method 1: directly call the solve routine# #################################################
+# Advantage: No network layer needed. Disadvantage: For each solve, the tables have to be loaded which takes a few     #
+# seconds for each new solve.                                                                                          #
+########################################################################################################################
+
 #  Uncomment this part if you want to use method 1
 """
 import solver as sv
-a = sv.solve(cubestring)  # solve with a maximum of 20 moves and a timeout of 2 seconds for example
+a = sv.solve(cubestring, 20, 2)  # solve with a maximum of 20 moves and a timeout of 2 seconds for example
+print(a)
+a = sv.solve(cubestring, 18, 5)  # solve with a maximum of 18 moves and a timeout of 5 seconds for example
 print(a)
 quit()
 """
@@ -15,18 +21,21 @@ quit()
 
 
 # ############################### Method 2 a/b: Start the cubesolving-server# ##########################################
+# Advantage: Tables have to be loaded only once when the server starts. Disadvantage: Network layer must be present.   #
+########################################################################################################################
+
 #----------------------------------------------------------------------------------------------------------------------
 # Method 2a: Start the server from inside a Python script:
 import start_server
 from threading import Thread
-background_thread = Thread(target=start_server.start, args=(8080,))
+background_thread = Thread(target=start_server.start, args=(8080, 20, 2))
 background_thread.start()
 # Server listens now on port 8080, maxlength 20 moves, timeout 2 seconds
 # ----------------------------------------------------------------------------------------------------------------------
 
 # ----------------------------------------------------------------------------------------------------------------------
-# Method 2b: Start the server from a terminal with parameter for port:
-# python start_server.py 8080
+# Method 2b: Start the server from a terminal with parameters for port, maxlength and timeout:
+# python start_server.py 8080 20 2
 # ----------------------------------------------------------------------------------------------------------------------
 
 
@@ -34,14 +43,14 @@ background_thread.start()
 
 # ----------------------------------------------------------------------------------------------------------------------
 # 1. With a webbrowser, if the server runs on the same machine on port 8080
-# http://localhost:8080/FFBLBRDLDUBRRFDDLRLUUUFB
+# http://localhost:8080/DUUBULDBFRBFRRULLLBRDFFFBLURDBFDFDRFRULBLUFDURRBLBDUDL
 # With a webbrowser, if the server runs on server myserver.com, port 8081
-# http://myserver.com:8081/FFBLBRDLDUBRRFDDLRLUUUFB
+# http://myserver.com:8081/DUUBULDBFRBFRRULLLBRDFFFBLURDBFDFDRFRULBLUFDURRBLBDUDL
 # ----------------------------------------------------------------------------------------------------------------------
 
 # ----------------------------------------------------------------------------------------------------------------------
 # 2. With netcat, if the server runs on the same machine on port 8080
-# echo FFBLBRDLDUBRRFDDLRLUUUFB | nc localhost 8080
+# echo DUUBULDBFRBFRRULLLBRDFFFBLURDBFDFDRFRULBLUFDURRBLBDUDL | nc localhost 8080
 # ----------------------------------------------------------------------------------------------------------------------
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -55,3 +64,4 @@ import client_gui
 # ----------------------------------------------------------------------------------------------------------------------
 
 ########################################################################################################################
+
